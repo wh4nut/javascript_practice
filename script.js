6b01e222b613b29b3f07a8702614306f21191321
Sudakov Kyrilo <wh4nut@gmail.com>
2025-02-04 20:03:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+// let number = 120.5;
+// console.log(-4 / 0);
+// console.log("string ", " string" * 3);
+
+// const persone = "Alex 5";
+// const bool = true;
+// // console.log(somthing);
+// let und;
+// console.log(und);
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   ismarried: false,
+// };
+
+// console.log(obj["name"]);
+
+// let arr = ["some.png", "vagination-arrest.org", 6, {}, []];
+// console.log(arr[1]);
+
+// const arr = [1, 2, 3];
+// arr[10] = "James";
+// console.log(arr[10]);
+// console.log(arr);
+
+// const arrObj = {
+//   0: 1,
+//   1: 2,
+//   2: 3,
+// };
+
+// arrObj.newSkill = "Blink";
+// arrObj["newSkill"] = "Silk";
+
+// console.log(arrObj["newSkill"]);
+// console.log(arrObj.newSkill);
+
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+//   Kyrylo: {
+//     age: 24,
+//     work: {},
+//     life: [{ friends: true }, { love: true }, { family: true }],
+//   },
+// };
+
+// console.table(obj);
+// alert("Hi there!");
+
+// const result = confirm("You will suck!");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "Yes");
+// console.log(typeof answer);
+
+// alert("Nice!");
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your secondname?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// console.log(typeof answers);
+// console.log(typeof null);
+
+// const category = "toys";
+// const user = "Ivan";
+// console.log(`https://someurl.com/ + ${category}/5`);
+
+// alert(`Hi there, ${user}`);
+
+console.log("arr" + " - object");
+console.log(4 + "5");
+console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+// ++incr;
+// --decr;
+console.log(incr++);
+console.log(decr--);
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+console.log(2 * 4 == 8);
+console.log(2 * 4 == "8");
+console.log(2 * 4 === "8");
+console.log(2 + 2 * 2 === 8);
+
+const checked = true,
+  close = false;
+
+console.log(checked && close);
+console.log(checked || close);
+console.log(checked || !close);
